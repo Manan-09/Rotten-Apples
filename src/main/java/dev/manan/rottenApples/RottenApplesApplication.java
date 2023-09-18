a1ea3c3201ed3f5fedbc4f1cee135538be1640ad
Manan Solanki <mananrsolanki@gmail.com>
2023-09-19 02:09:05 +0530

    Initialize as maven project

---
+++ src/main/java/dev/manan/rottenApples/RottenApplesApplication.java
@@ -0,0 +1,13 @@
+package dev.manan.rottenApples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RottenApplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RottenApplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/manan/rottenApples/RottenApplesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.manan.rottenApples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RottenApplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
