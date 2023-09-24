cee980f397b01424ae2877cfd9e6cb5e890cd19a
Manan Solanki <mananrsolanki@gmail.com>
2023-09-25 00:02:48 +0530

    Update movie entity id to movieId

--- src/main/java/dev/manan/rottenApples/repo/MovieRepository.java
+++ src/main/java/dev/manan/rottenApples/repo/MovieRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+public interface MovieRepository extends MongoRepository<Movie, String> {
 
     Optional<Movie> findByMovieId(String movieId);
 }
