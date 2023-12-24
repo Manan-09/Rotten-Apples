ba25a4ceea8592e6cc21403be42f6189bed303b3
Manan Solanki <mananrsolanki@gmail.com>
2023-12-25 02:55:59 +0530

    Add constrain for movie review

--- src/main/java/dev/manan/rottenApples/dto/ReviewRequestDTO.java
+++ src/main/java/dev/manan/rottenApples/dto/ReviewRequestDTO.java
@@ -1,5 +1,7 @@
 package dev.manan.rottenApples.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +12,7 @@ import lombok.NoArgsConstructor;
 public class ReviewRequestDTO {
     private String movieId;
     private String body;
+    @Min(1)
+    @Max(100)
     private Integer score;
 }
